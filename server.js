e4226595b6e898b668283dc1c0cf7644ee9a2289
Hins <naruto_khor1987@hotmail.com>
2016-07-21 06:40:47 +0000

    add datestring listener

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var moment = require('moment');
 
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
 });
 
 app.listen(8080, function () {
