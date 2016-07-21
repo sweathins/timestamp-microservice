47715469ebe0e9dd9b4a2d07093efc4ee03c8d42
Hins <naruto_khor1987@hotmail.com>
2016-07-21 06:59:27 +0000

    Add Port

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/:datestring', function(req,res) {
 
 });
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!');
 });
